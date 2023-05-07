811dbb79ac5b70844772d1c4951b1cf9c75963f0
Tran Toan <tofbest267@gmail.com>
2023-05-08 03:03:29 +0700

    first init

---
+++ src/components/Example.jsx
@@ -0,0 +1,11 @@
+import "./Example.scss";
+
+function Example() {
+  return (
+    <div>
+      <h1>Example</h1>
+    </div>
+  );
+}
+
+export default Example;
---
+++ src/pages/LandingPage.jsx
@@ -0,0 +1,12 @@
+import Example from "../components/Example";
+
+function LandingPage() {
+  return (
+    <div className="landing-page">
+      <h1>My Portfolio</h1>
+      <Example />
+    </div>
+  );
+}
+
+export default LandingPage;
